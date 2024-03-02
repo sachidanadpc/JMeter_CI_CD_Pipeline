000fee520e4a60c34e80e4352ccd09654ae5814d
sachidanadpc <49983832+sachidanadpc@users.noreply.github.com>
2024-03-02 14:19:25 +0530

    Script to install Docker

---
+++ scripts/install_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package index
+sudo apt-get update
+
+# Install required packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable Docker repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Update the package index again
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce
