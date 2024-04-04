1eac28c152d50ae15e71abb5698e301ede75b59e
sachidanadpc <49983832+sachidanadpc@users.noreply.github.com>
2024-04-04 21:37:04 +0530

    Update setup_kubernetes.sh

--- scripts/setup_kubernetes.sh
+++ scripts/setup_kubernetes.sh
@@ -2,10 +2,10 @@
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-# Update package index
-sudo apt-get update
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-# Install Kubernetes components
+sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 
 # Initialize Kubernetes cluster
