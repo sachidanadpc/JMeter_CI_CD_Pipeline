e2e6d5911729750e39f55d6edb2e4ab51572290f
sachidanadpc <49983832+sachidanadpc@users.noreply.github.com>
2024-04-06 21:35:30 +0530

    Update run_jmeter_tests.sh

--- scripts/run_jmeter_tests.sh
+++ scripts/run_jmeter_tests.sh
@@ -2,14 +2,20 @@
 
 # Ensure JMeter test script is provided as an argument
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <jmeter_test_script.jmx>"
+    echo "Usage: $0 <my_test_script.jmx>"
     exit 1
 fi
 
+# Define timestamp
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+
 # Run JMeter test and generate test results
-docker run --rm -v $(pwd)/reports:/reports -v $(pwd)/$1:/$1 justb4/jmeter -n -t /$1 -l /reports/test_results_$(date +"%Y%m%d_%H%M%S").jtl -e
+docker run --rm -v $(pwd)/reports:/reports -v $(pwd)/$1:/$1 justb4/jmeter -n -t /$1 -l /reports/test_results_${TIMESTAMP}.jtl -e
+
+# Set read permission on test results file
+chmod +r /path/to/test_results_${TIMESTAMP}.jtl
 
 # Generate HTML report index.html
-docker run --rm -v $(pwd)/reports:/reports justb4/jmeter -g /reports/test_results_$(date +"%Y%m%d_%H%M%S").jtl -o /reports/html_report
+docker run --rm -v $(pwd)/reports:/reports justb4/jmeter -g /reports/test_results_${TIMESTAMP}.jtl -o /reports/html_report
 
 echo "JMeter test execution completed. HTML report generated at $(pwd)/reports/html_report/index.html"
