807430dcf47d5051d4c4b3f487b04413c7a9c5a0
sachidanadpc <49983832+sachidanadpc@users.noreply.github.com>
2024-04-06 21:13:25 +0530

    Update run_jmeter_tests.sh

--- scripts/run_jmeter_tests.sh
+++ scripts/run_jmeter_tests.sh
@@ -6,10 +6,10 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Run JMeter test
-docker run --rm -v $(pwd)/reports:/reports -v $(pwd)/$1:/$1 jmeter -n -t /$1 -l /reports/test_results.jtl -e -o /reports/html_report
+# Run JMeter test and generate test results
+docker run --rm -v $(pwd)/reports:/reports -v $(pwd)/$1:/$1 justb4/jmeter -n -t /$1 -l /reports/test_results_$(date +"%Y%m%d_%H%M%S").jtl -e
 
 # Generate HTML report index.html
-docker run --rm -v $(pwd)/reports:/reports jmeter sh -c 'jmeter -g /reports/test_results.jtl -o /reports/html_report'
+docker run --rm -v $(pwd)/reports:/reports justb4/jmeter -g /reports/test_results_$(date +"%Y%m%d_%H%M%S").jtl -o /reports/html_report
 
 echo "JMeter test execution completed. HTML report generated at $(pwd)/reports/html_report/index.html"
