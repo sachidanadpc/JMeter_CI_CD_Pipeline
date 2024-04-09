16df04c4d20ddbda4252878719e7a755b5c0f2db
sachidanadpc <49983832+sachidanadpc@users.noreply.github.com>
2024-04-09 20:59:46 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ export TARGET_PORT="80"
 export TARGET_PATH="/kaarten.html"
 export TARGET_KEYWORD="Kaartdiensten"
 
-T_DIR=tests_scripts
+T_DIR=test_scripts
 
 # Reporting dir: start fresh
 R_DIR=${T_DIR}/report
